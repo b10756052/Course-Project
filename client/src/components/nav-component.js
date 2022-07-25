9316a1a1a5fc688c0b7d3bedca7a93344b427dd8
B10756052 <B10756052@gmail.com>
2022-07-25 21:15:39 +0800

    navbar

--- client/src/components/nav-component.js
+++ client/src/components/nav-component.js
@@ -15,7 +15,7 @@ const NavComponent = (props) => {
   return (
     <div>
       <nav>
-        <nav className="navbar  navbar-light bg-light">
+        <nav className="navbar navbar-expand-md navbar-expand-xs navbar-light bg-light">
           <div className="container-fluid">
             {/* navbarNav */}
             <div
